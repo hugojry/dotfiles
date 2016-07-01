f53ea328e42f34c93fc9dbff25fc3a48aca48b55
Hugo Young <hyoung1@sheffield.ac.uk>
2016-07-01 22:02:03 +0100

    added themer, colors and wallpapers

---
+++ themer/base16ify.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+
+order = [
+  'base00',
+  'base08',
+  'base0B',
+  'base0A',
+  'base0D',
+  'base0E',
+  'base0C',
+  'base05',
+  'base03',
+  'base08',
+  'base0B',
+  'base0A',
+  'base0D'
+]
