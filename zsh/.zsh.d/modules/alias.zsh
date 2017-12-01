fa4ac70811453369acf416c412e72e5e0bbc5141
Hugo Young <hybas3@gmail.com>
2017-12-01 12:44:27 +0000

    Set term to xterm in tmux for vim and remove nvim alias

--- zsh/.zsh.d/modules/alias.zsh
+++ zsh/.zsh.d/modules/alias.zsh
@@ -1,11 +1,5 @@
 # xorg
 alias xup="xrdb ~/.Xresources"
-
-# vim
-if type "nvim" 2>&1 >/dev/null; then
-    alias vim="nvim"
-fi 
 alias svim="'vim' --servername VIM"
-
 #rbenv
 alias rb='eval "$(rbenv init -)"'
