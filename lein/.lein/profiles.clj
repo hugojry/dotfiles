e2c769e5551ca7fbea4d88e3f895bb892d0490d6
Hugo Young <hybas3@gmail.com>
2018-04-16 20:19:46 +0100

    Add kibit to lein profile

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [; necessary for fireplace
                   [cider/cider-nrepl "0.15.1"]
                   ; linting and static analysis
-                  [jonase/eastwood "0.2.5"]]}}
+                  [jonase/eastwood "0.2.5"]
+                  [lein-kibit "0.1.5"]]}}
