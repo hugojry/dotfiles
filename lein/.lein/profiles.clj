4a9297fb93eae3f8a1e6ed0c452242a7ce5bb9b2
Hugo Young <hybas3@gmail.com>
2018-07-02 21:29:53 +0100

    Bump cider-nrepl version

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [; necessary for fireplace
-                  [cider/cider-nrepl "0.16.0"]
+                  [cider/cider-nrepl "0.17.0"]
                   ; linting and static analysis
                   [jonase/eastwood "0.2.5"]
                   [lein-kibit "0.1.5"]]}}
