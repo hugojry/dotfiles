e645af16ffb526f5c2ba3f54a96f1ed70f6b983b
Hugo Young <hybas3@gmail.com>
2018-10-17 10:16:17 +0100

    Add rebel-readline to lein dependencies

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -2,4 +2,5 @@
                   [cider/cider-nrepl "0.17.0"]
                   ; linting and static analysis
                   [jonase/eastwood "0.2.5"]
-                  [lein-kibit "0.1.5"]]}}
+                  [lein-kibit "0.1.5"]]
+        :dependencies [[com.bhauman/rebel-readline "0.1.4"]]}}
