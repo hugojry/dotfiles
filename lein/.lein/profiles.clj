4a78741f255412d6ca5f841fb07b028355ac6348
Hugo Young <hybas3@gmail.com>
2019-04-10 11:07:08 +0100

    Remove lein

--- lein/.lein/profiles.clj
+++
@@ -1,6 +0,0 @@
-{:user {:plugins [; necessary for fireplace
-                  [cider/cider-nrepl "0.17.0"]
-                  ; linting and static analysis
-                  [jonase/eastwood "0.2.5"]
-                  [lein-kibit "0.1.5"]]
-        :dependencies [[com.bhauman/rebel-readline "0.1.4"]]}}
