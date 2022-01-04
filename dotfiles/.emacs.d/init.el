57e61c558f1b66f1c4bfe12049adf62b40c0ff6b
Hugo Young <hybas3@gmail.com>
2022-01-04 11:01:54 +0000

    Don't show trailing whitespace

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -25,8 +25,6 @@
 (require 'lisp-indent-function)
 (setq lisp-indent-function #'Fuco1/lisp-indent-function)
 
-(setq-default show-trailing-whitespace t)
-
 (savehist-mode)
 
 (setq column-number-indicator-zero-based nil)
