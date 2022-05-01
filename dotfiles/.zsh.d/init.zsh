217035890a83726ad58311c01abfeaed5449cc8e
Hugo Young <hybas3@gmail.com>
2022-05-01 01:43:32 +0100

    Amend fpath before starting completions

--- dotfiles/.zsh.d/init.zsh
+++ dotfiles/.zsh.d/init.zsh
@@ -1,2 +1,7 @@
+if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+fi
+
 autoload -Uz compinit && compinit
 autoload -Uz colors && colors
