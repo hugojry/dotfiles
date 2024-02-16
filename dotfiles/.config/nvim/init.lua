1671426be4d64371767ec80cdb334265278086a7
Hugo Young <hybas3@gmail.com>
2024-02-16 20:33:37 +0000

    Remove weird winblend

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -193,7 +193,6 @@ vim.keymap.set('n', '<localleader>bb', require('telescope.builtin').buffers, { d
 vim.keymap.set('n', '<C-s>', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
     previewer = false,
   })
 end, { desc = '[<C-s>] Fuzzily search in current buffer' })
