2fec6e3ed8a456e7d73380c568e3693e00586caf
Hugo Young <hybas3@gmail.com>
2024-02-25 23:10:51 +0000

    Don't shadow diagnostics mappings in clojure

--- dotfiles/.config/nvim/after/ftplugin/clojure.lua
+++ dotfiles/.config/nvim/after/ftplugin/clojure.lua
@@ -2,3 +2,12 @@
 vim.keymap.set('n', '<localleader>e', 'cp', { remap = true, buffer = true })
 vim.keymap.set('n', '<localleader>f', 'cpaF', { remap = true , buffer = true})
 vim.keymap.set('n', '<localleader>k', ':%Eval<cr>', { buffer = true })
+
+-- fireplace is using these mappings, so take them back
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'FireplaceActivate',
+    callback = function()
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { buffer = true })
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { buffer = true })
+    end
+})
