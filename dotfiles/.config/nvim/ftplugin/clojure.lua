7e46c7a842a0ddf88fb4f36a3cd9ba32b3613ac0
Hugo Young <hybas3@gmail.com>
2024-02-25 20:49:43 +0000

    Make clojure mappings buffer local

--- dotfiles/.config/nvim/ftplugin/clojure.lua
+++ dotfiles/.config/nvim/ftplugin/clojure.lua
@@ -1,4 +1,4 @@
 -- Clojure mappings
-vim.keymap.set('n', '<localleader>e', 'cp', { remap = true })
-vim.keymap.set('n', '<localleader>f', 'cpaF', { remap = true })
-vim.keymap.set('n', '<localleader>k', ':%Eval<cr>')
+vim.keymap.set('n', '<localleader>e', 'cp', { remap = true, buffer = true })
+vim.keymap.set('n', '<localleader>f', 'cpaF', { remap = true , buffer = true})
+vim.keymap.set('n', '<localleader>k', ':%Eval<cr>', { buffer = true })
