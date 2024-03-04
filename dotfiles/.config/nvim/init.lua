93bdf155db20c678e9ab1687b2f2bc1e31d9ab46
Hugo Young <hybas3@gmail.com>
2024-03-04 15:03:04 +0000

    Custom vim-sexp mappings

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -106,7 +106,6 @@ require('lazy').setup({
 
   -- Lisp
   'guns/vim-sexp',
-  'tpope/vim-sexp-mappings-for-regular-people',
 
   -- Clojure
   'tpope/vim-fireplace',
@@ -349,6 +348,36 @@ end
 vim.keymap.set('n', '<localleader>c', ':make<cr>')
 vim.keymap.set('n', '<localleader>C', set_makeprg)
 vim.keymap.set('n', '<localleader>bk', ':b# | bd#<cr>')
+
+local buffer_map = function(modes, lhs, rhs)
+  vim.keymap.set(modes, lhs, rhs, { buffer = true })
+end
+
+local setup_sexp_mappings = function()
+  if not vim.g.sexp_loaded then
+    return
+  end
+
+  vim.g.sexp_insert_after_wrap = false
+  buffer_map('n', '<localleader>r', '<Plug>(sexp_swap_element_forward)')
+  buffer_map('n', '<localleader>R', '<Plug>(sexp_swap_element_backward)')
+  buffer_map({ 'n', 'o', 'x' }, 'L', '<Plug>(sexp_move_to_next_element_head)')
+  buffer_map({ 'n', 'o', 'x' }, 'H', '<Plug>(sexp_move_to_prev_element_head)')
+  buffer_map('n', '<c', '<Plug>(sexp_capture_prev_element)')
+  buffer_map('n', '>c', '<Plug>(sexp_capture_next_element)')
+  buffer_map('n', '<e', '<Plug>(sexp_emit_tail_element)')
+  buffer_map('n', '>e', '<Plug>(sexp_emit_head_element)')
+  buffer_map({ 'n', 'v' }, '<localleader>(', '<Plug>(sexp_round_head_wrap_element)')
+  buffer_map({ 'n', 'v' }, '<localleader>[', '<Plug>(sexp_square_head_wrap_element)')
+  buffer_map({ 'n', 'v' }, '<localleader>{', '<Plug>(sexp_curly_head_wrap_element)')
+end
+
+local sexp_mappings_group = vim.api.nvim_create_augroup("sexp_mappings_for_hy", {})
+vim.api.nvim_create_autocmd("FileType", {
+  group = sexp_mappings_group,
+  pattern = vim.g.sexp_filetypes or "lisp,scheme,clojure",
+  callback = setup_sexp_mappings
+})
 
 vim.g.netrw_banner = 0
 vim.g.netrw_list_hide = '\\./,\\.\\./'
