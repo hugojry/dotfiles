a3f8aabefa1888e969e7b3e5f6e808453c97f3b6
Hugo Young <hybas3@gmail.com>
2024-06-16 12:59:14 +0100

    Split right and below

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -340,4 +340,7 @@ vim.o.foldmethod = 'expr'
 vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.o.foldlevel = 99
 
+vim.o.splitright = true
+vim.o.splitbelow = true
+
 -- vim: ts=2 sts=2 sw=2 et
