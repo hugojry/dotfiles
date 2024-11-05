a5f86ad1d0a526029d76780534baed35273e156d
Hugo Young <hybas3@gmail.com>
2024-11-05 13:26:03 +0000

    Remove pointless diagnostic stuff

--- dotfiles/.config/nvim/after/ftplugin/clojure.lua
+++ dotfiles/.config/nvim/after/ftplugin/clojure.lua
@@ -3,17 +3,6 @@ vim.keymap.set('n', '<localleader>e', 'cp', { remap = true, buffer = true })
 vim.keymap.set('n', '<localleader>f', 'cpaF', { remap = true , buffer = true})
 vim.keymap.set('n', '<localleader>k', ':%Eval<cr>', { buffer = true })
 
-local util = require('util')
-
--- fireplace is using these mappings, so take them back
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'FireplaceActivate',
-  callback = function()
-    vim.keymap.set('n', '[d', util.goto_prev_diagnostic, { buffer = true })
-    vim.keymap.set('n', ']d', util.goto_next_diagnostic, { buffer = true })
-  end
-})
-
 local threading = require('clojure.threading')
 vim.keymap.set('n', '<localleader>rf', threading.thread_first, { buffer = true })
 vim.keymap.set('n', '<localleader>rl', threading.thread_last, { buffer = true })
