9f845d450e71beb67e48494138af72b2f6049965
Hugo Young <hybas3@gmail.com>
2026-03-25 14:23:41 +0000

    init.el minus niche language stuff

---
+++ dotfiles/.emacs.d/init-min.el
@@ -0,0 +1,281 @@
+;;; init.el --- Emacs configuration -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+(setq custom-file "~/.emacs.d/custom.el")
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+(require 'dabbrev)
+(require 'dired)
+(require 'package)
+(require 'project)
+(require 'recentf)
+(require 'use-package)
+
+(require 'lisp-indent-function)
+(require 'wsl)
+
+(setq ring-bell-function 'ignore
+      inhibit-startup-message t
+      gc-cons-threshold 100000000
+      indent-tabs-mode nil
+      case-fold-search nil
+      show-paren-delay 0
+      dabbrev-case-fold-search nil
+      dired-dwim-target t
+      eldoc-echo-area-use-multiline-p nil
+      backup-directory-alist '(("." . "~/.emacs.d/backup/"))
+      recentf-max-saved-items 1000
+      recentf-max-menu-items 1000
+      use-package-always-ensure t
+      use-package-always-defer t
+      lisp-indent-function #'Fuco1/lisp-indent-function
+      project-vc-extra-root-markers '(".project" ".projectile"))
+
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(blink-cursor-mode -1)
+
+(savehist-mode)
+(column-number-mode)
+(save-place-mode)
+(show-paren-mode)
+(recentf-mode)
+
+(use-package diminish
+  :demand t
+  :config
+  (diminish 'eldoc-mode)
+  (with-eval-after-load 'autorevert
+    (diminish 'auto-revert-mode)))
+
+(use-package general
+  :demand t
+  :config
+  (general-evil-setup)
+
+  (general-create-definer general-spc
+    :states 'normal
+    :keymaps 'override
+    :prefix "SPC")
+
+  (general-create-definer general-comma
+    :keymaps 'override
+    :states 'normal
+    :prefix ",")
+
+  (general-spc
+    "u" #'universal-argument
+    "-" #'negative-argument
+    "p" project-prefix-map
+    "f f" #'find-file)
+
+  (general-def emacs
+    "C-6" #'previous-buffer)
+
+  (general-def normal
+    "-" #'dired-jump
+    "_" #'dired-jump-other-window)
+
+  (general-def "C-h M-k" #'describe-keymap)
+
+  (general-comma
+    "b b" #'switch-to-buffer
+    "b k" #'kill-buffer)
+
+  (general-def normal emacs-lisp-mode-map
+    "K" #'describe-symbol)
+
+  (general-def normal (emacs-lisp-mode-map lisp-interaction-mode-map)
+    ", f" #'eval-defun
+    ", k" #'eval-buffer)
+
+  (general-def (normal insert) lisp-interaction-mode-map
+    "C-j" #'eval-print-last-sexp))
+
+(use-package evil
+  :demand t
+  :general
+  (general-comma "C" #'hy/bind-command)
+  (general-def (evil-normal-state-map evil-visual-state-map) "u" #'undo)
+  (general-def normal (emacs-lisp-mode-map lisp-interaction-mode-map)
+    ", e" #'hy/evil-eval)
+  :init
+  (setq evil-want-integration t
+        evil-want-keybinding nil
+        evil-want-C-u-scroll t
+        evil-respect-visual-line-mode t
+        evil-search-module 'evil-search
+        evil-symbol-word-search t
+        evil-split-window-below t
+        evil-vsplit-window-right t)
+  :config
+  (defalias #'forward-evil-word #'forward-evil-symbol)
+
+  (evil-define-operator hy/evil-eval (beg end type)
+    :move-point nil
+    (eval-region beg end t))
+
+  (defun hy/nohighlight ()
+    (when (memq this-command evil-motions)
+      (evil-ex-nohighlight)))
+
+  (add-hook 'post-command-hook #'hy/nohighlight)
+
+  (defun hy/bind-command (command key)
+    (interactive "sShell command: \nKKey: ")
+    (let ((f (lambda ()
+               (interactive)
+               (shell-command command))))
+      (define-key evil-normal-state-map key f)))
+
+  (advice-add 'evil-visual-update-x-selection :override #'ignore)
+
+  (evil-mode))
+
+(use-package evil-collection
+  :demand t
+  :general
+  (general-def normal evil-collection-unimpaired-mode-map
+    "[d" #'evil-collection-unimpaired-previous-error
+    "]d" #'evil-collection-unimpaired-next-error
+    "[q" nil "]q" nil)
+  :diminish evil-collection-unimpaired-mode
+  :config
+  (advice-add 'evil-collection-vterm-setup :after
+              (lambda ()
+                (evil-set-initial-state 'vterm-mode 'emacs)))
+
+  (evil-collection-init))
+
+(use-package magit
+  :general
+  (general-spc "g g" #'magit-status)
+  :init
+  (setq magit-bury-buffer-function 'magit-mode-quit-window)
+  (with-eval-after-load 'project
+    (define-key project-prefix-map "m" #'magit-project-status)
+    (add-to-list 'project-switch-commands '(magit-project-status "Magit") t))
+  (evil-ex-define-cmd "G" #'magit-status))
+
+(defconst hy/lisp-mode-hooks
+  '(emacs-lisp-mode-hook
+    inferior-lisp-mode-hook
+    eval-expression-minibuffer-setup-hook
+    lisp-mode-hook))
+
+(use-package paredit
+  :ghook hy/lisp-mode-hooks
+  :general
+  (general-def normal paredit-mode-map
+    ", (" #'paredit-wrap-round
+    ", [" #'paredit-wrap-square
+    ", {" #'paredit-wrap-curly
+    ", O" #'paredit-raise-sexp
+    ", @" #'paredit-splice-sexp)
+  (general-def paredit-mode-map
+    "RET" #'paredit-newline)
+  :init
+  (add-hook 'paredit-mode-hook (lambda () (electric-indent-local-mode 0)))
+  :config
+  (define-minor-mode paredit-eval-expression-mode
+    "Fixes RET in minibuffer with paredit"
+    :keymap (make-sparse-keymap)
+    (define-key paredit-eval-expression-mode-map
+      (kbd "RET")
+      #'exit-minibuffer))
+
+  (add-hook 'eval-expression-minibuffer-setup-hook
+            #'paredit-eval-expression-mode))
+
+(use-package lispyville
+  :diminish
+  :ghook 'paredit-mode-hook
+  :general
+  (general-def normal lispyville-mode-map
+    ", R" #'lispyville-drag-backward
+    ", r" #'lispyville-drag-forward)
+  (general-def (normal visual motion) lispyville-mode-map
+    "(" #'lispyville-backward-up-list
+    ")" #'lispyville-up-list
+    "H" #'lispyville-backward-sexp
+    "L" #'lispyville-forward-sexp)
+  :config
+  (lispyville-set-key-theme
+   '(operators
+     text-objects
+     slurp/barf-cp
+     c-w)))
+
+(use-package company
+  :diminish
+  :init
+  (global-company-mode)
+  (company-tng-mode))
+
+(use-package ivy
+  :diminish
+  :init
+  (setq ivy-extra-directories nil)
+  :config
+  (ivy-mode))
+
+(use-package counsel
+  :diminish
+  :init
+  (counsel-mode)
+  :general
+  (general-spc "f r" #'counsel-recentf))
+
+(use-package swiper
+  :general
+  ("C-s" #'swiper))
+
+(use-package exec-path-from-shell
+  :init
+  (exec-path-from-shell-initialize))
+
+;; Sleuth
+(use-package dtrt-indent
+  :diminish
+  :init
+  (dtrt-indent-global-mode))
+
+(use-package lsp-mode
+  :general
+  (general-def normal lsp-mode-map
+    "K" #'lsp-describe-thing-at-point
+    "g r" #'lsp-find-references)
+  :init
+  (setq lsp-headerline-breadcrumb-enable nil)
+  :config
+  (evil-define-key 'normal lsp-mode-map (kbd "SPC l") lsp-command-map))
+
+(use-package lsp-ivy
+  :commands lsp-ivy-workspace-symbol)
+
+(use-package flycheck
+  :general
+  (general-spc flycheck-mode-map
+    "e" #'flycheck-display-error-at-point)
+  :init
+  (global-flycheck-mode)
+  :config
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+  (evil-define-key 'normal flycheck-mode-map (kbd "SPC !") flycheck-command-map))
+
+(use-package typescript-mode
+  :hook (typescript-mode . lsp))
+
+(use-package lsp-pyright
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-pyright)
+                         (lsp))))
+
+(require 'hy-frame)
+
+(provide 'init)
+;;; init.el ends here
